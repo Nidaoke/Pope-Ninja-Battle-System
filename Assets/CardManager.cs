2bb08f25080734b97f926cd7b6a190b896efa7bd
Nidaoke <jnidaokefantagefacebook27@gmail.com>
2015-08-31 16:45:01 -0400

    Updated card manager.
    Also cleaned up code to make it easier to add more cards.

--- Assets/CardManager.cs
+++ Assets/CardManager.cs
@@ -1,24 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CardManager : MonoBehaviour {
+public class CardManager : MonoBehaviour
+{
 
-	public GameObject PreviousCard;
-	public GameObject CurrentCard;
-	public GameObject NextCard;
+	#region variables
 
-	public int arrayElement = 0;
+	[Header("Card Holders")]
+
+	public GameObject
+		Card_3;
+	public GameObject Card_2;
+	public GameObject Card_1;
+	public GameObject Card1;
+	public GameObject Card2;
+	public GameObject Card3;
+	public GameObject Card4;
+	[Header("Stored Card Variables")]
+	public int
+		arrayElement = 0;
 	public string selectedCard;
+	[Header("Card List")]
+	public string[]
+		cardList = new string[3];
+	public Sprite[] cardImages = new Sprite[3];
 
-	public string [] cardList = new string[3];
+	#endregion
 
-	public Sprite [] cardImages = new Sprite[3];
-
-	void Start(){
+	void Start ()
+	{
 		
 	}
 
-	void Update(){
+	void Update ()
+	{
+
+		//Debug.Log (cardList.Length);
 
 		#region changeCardValue
 
@@ -26,10 +43,10 @@ public class CardManager : MonoBehaviour {
 
 		if (Input.GetKeyDown (KeyCode.RightArrow)) {
 
-			if((arrayElement + 1) < cardList.Length){
+			if ((arrayElement + 1) < cardList.Length) {
 
 				arrayElement ++;
-			}else{
+			} else {
 
 				arrayElement = 0;
 			}
@@ -37,10 +54,10 @@ public class CardManager : MonoBehaviour {
 
 		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
 			
-			if(arrayElement > 0){
+			if (arrayElement > 0) {
 				
 				arrayElement --;
-			}else{
+			} else {
 				
 				arrayElement = cardList.Length - 1;
 			}
@@ -50,29 +67,88 @@ public class CardManager : MonoBehaviour {
 
 		#region changeCardImages
 
-		CurrentCard.GetComponent<SpriteRenderer> ().sprite = cardImages[arrayElement];
+		Card1.GetComponent<SpriteRenderer> ().sprite = cardImages [arrayElement];
 
-		if((arrayElement + 1) < cardList.Length){
-			
-			NextCard.GetComponent<SpriteRenderer> ().sprite = cardImages[arrayElement + 1];
-		}else{
-			
-			NextCard.GetComponent<SpriteRenderer> ().sprite = cardImages[0];
-		}
+		#region card4
 
-		if(arrayElement > 0){
-			
-			PreviousCard.GetComponent<SpriteRenderer> ().sprite = cardImages[arrayElement - 1];
-		}else{
-			
-			PreviousCard.GetComponent<SpriteRenderer> ().sprite = cardImages[cardList.Length - 1];
+		if ((arrayElement + 3) < cardList.Length) {
+
+			Card4.GetComponent<SpriteRenderer> ().sprite = cardImages [arrayElement + 3];
+		} else {
+
+			Card4.GetComponent<SpriteRenderer> ().sprite = cardImages [arrayElement - 1];
 		}
 
 		#endregion
 
-		if(Input.GetKeyDown(KeyCode.Space)){
+		#region card3
 
-			Debug.Log("Used Card: " + selectedCard);
+		if ((arrayElement + 2) < cardList.Length) {
+
+			Card3.GetComponent<SpriteRenderer> ().sprite = cardImages [arrayElement + 2];
+		} else {
+
+			Card3.GetComponent<SpriteRenderer> ().sprite = cardImages [arrayElement - 2];
+		}
+
+		#endregion
+
+		#region card2
+
+		if ((arrayElement + 1) < cardList.Length) {
+			
+			Card2.GetComponent<SpriteRenderer> ().sprite = cardImages [arrayElement + 1];
+		} else {
+			
+			Card2.GetComponent<SpriteRenderer> ().sprite = cardImages [0];
+		}
+
+		#endregion
+
+		#region card-1
+
+		if (arrayElement > 0) {
+			
+			Card_1.GetComponent<SpriteRenderer> ().sprite = cardImages [arrayElement - 1];
+		} else {
+			
+			Card_1.GetComponent<SpriteRenderer> ().sprite = cardImages [cardList.Length - 1];
+		}
+
+		#endregion
+
+		#region card-2
+
+
+		if((arrayElement - 1 > 0)){
+
+			Card_2.GetComponent<SpriteRenderer> ().sprite = cardImages[arrayElement - 2];
+		}else{
+
+			Card_2.GetComponent<SpriteRenderer> ().sprite = cardImages[arrayElement + 2];
+		}
+
+		#endregion
+
+		#region card-3
+
+		if ((arrayElement - 2 > 0)) {
+			
+			Card_3.GetComponent<SpriteRenderer> ().sprite = cardImages [arrayElement - 3];
+		} else {
+
+				
+				Card_3.GetComponent<SpriteRenderer> ().sprite = cardImages [arrayElement + 1];
+
+		}
+
+		#endregion
+
+		#endregion
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+			Debug.Log ("Used Card: " + selectedCard);
 		}
 	}
 }
